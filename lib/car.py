ab4e3b23513b4b574f8b6e7b1529d492a1a37765
Patricio <jpdibacco@gmail.com>
2021-04-16 17:50:40 +0200

    henry's test ma files

--- lib/car.py
+++ lib/car.py
@@ -1,19 +1,23 @@
 # -*- coding: UTF-8 -*-
+import random
 import maya.cmds as cmds
+
 sc = cmds.internalVar(userScriptDir=True)
 #étape 1: create a path, e,g, a curve
 #print(sc+'tp_race_generator/fbx/couper.fbx')
  
 class carCreator():
+    listNumbers = list(range(250))
+    randomSelection = random.choice(listNumbers)
+    print(randomSelection)
     @classmethod
     def createCar(self):
-        couper = cmds.file(sc +'tp_race_generator/fbx/mercedes_rig.fbx', i = True, type = 'fbx')
-        cmds.scale(0.1,0.1,0.1, 'Car_Python', ws=True, a=True)
+        couper = cmds.file(sc +'tp_race_generator/ma/mercedes_rig.ma', i = True)
+        # cmds.scale(0.1,0.1,0.1, 'Car_Python', ws=True, a=True)
     @classmethod
     def animateCar(self):
         cmds.pathAnimation('ctrl_Car_python', c='curve1', stu = 0, etu = 120, f = False, fa='Z' )
         cmds.keyTangent( 'motionPath1_uValue', ott='linear', itt='linear' )
-        #meter random!
         cmds.setAttr('motionPath1.worldUpType',0)
         cmds.setAttr('motionPath1.upAxis', 1)
 
